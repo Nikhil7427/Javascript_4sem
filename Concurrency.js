c5c7bc2d08305d251940198dba6347817ed855ba
Nikhil7427 <nikhilmaurya1010@gmail.com>
2025-05-02 12:25:19 +0530

    async and await,event loop

--- Concurrency.js
+++ Concurrency.js
@@ -1,10 +1,11 @@
 //CONCURRENCY: Concurrency iS WHENEVER THINGS ARE HAPPENING AT THE SAME TIME  OR IN PARALLEL
 
-//CALLBACKS: it is a function that takes another function as an argument.
+//CALLBACKS: it is a function that takes another function as an argument.(which is then called when the rest of the intial function has finished)
 // CALLBACKS BECOME REALLY VALUABE IN AN asynchronous context,for example when one function is still waiting for the result of call to the database before calling the callback function the is going to process the data
 
 // function doSomething(callback){
 //     callback();
+//     console.log("hiiii");
 // }
 // function sayHi(){
 //     console.log("Hi!");
@@ -72,29 +73,81 @@
 //         console.log("Error",error);
 //     }
 // );
-const promise= new Promise((resolve,reject) =>{
-    // resolve("Succes!");
-    reject("Succes!");
-})
-.then(value => {
-    console.log(value);
-    return "we";
-})
-.then(value => {
-    console.log(value);
-    return "can";
-})
-.then(value => {
-    console.log(value);
-    return "chain";
-})
-.then(value => {
-    console.log(value);
-    return "promises";
-})
-.then(value => {
-    console.log(value);
-})
-.catch(value =>{
-    console.log(value);
-})
+// const promise= new Promise((resolve,reject) =>{
+//     resolve("Succes!");
+//     reject("Succes!");
+// })
+// .then(value => {
+//     console.log(value);
+//     return "we";
+// })
+// .then(value => {
+//     console.log(value);
+//     return "can";
+// })
+// .then(value => {
+//     console.log(value);
+//     return "chain";
+// })
+// .then(value => {
+//     console.log(value);
+//     return "promises";
+// })
+// .then(value => {
+//     console.log(value);
+// })
+// .catch(value =>{
+//     console.log(value);
+// })
+
+/* async and awiat */
+// //ASYNC AND AWAIT with the async keyword we can make a function return a promise.
+// //this makes the promise nicer to read and look a lot like synchronus(non-concurrent) code.
+// //we can use the more powerful await keyword to wait untill the proimse is done,await only works in an asynchronous. 
+
+// function saySomething(x) {
+//     return new Promise(resolve =>{
+//         setTimeout(()=>{
+//             resolve("something"+x);
+//         },2000);
+//     });
+// }
+// async function talk(x){
+//     const words= await saySomething(x);
+//     console.log(words);
+// }
+// talk(2);
+// talk(4);
+// talk(8);
+
+/*EVENT LOOP: */
+//javascript is a single threaded language. A thread in this context means a path of execution.
+// this single executer is the event loop
+//PROCESS: a program under execution.
+
+/*CALL STACK AND CALLBACK QUEUE */
+//callstack and call queue: the eveent loop is a process that is constantly
+//monitoring this call stack and whenever there are tasks to do ,the event loop
+//does them one by one, the tasks on top get executed first.
+
+// console.log("hi there");
+// add(4,5);
+// function add(x,y){
+//     return x+y;
+// }
+// console.log("Hi there");
+// setTimeout(()=> console.log("Sorry I'm late"),1000);
+// console.log(add(4,5));
+
+
+// the seTimeout() task gets outsourced to the browser's web API.
+// when it is done, this appears in special place: the callback queue.
+//whenn the call stack is empty , the event loop will check the callback queue for work to do.
+//if there is any callback waiting, they will be executed,one by one. after every action
+
+// function add(x,y){
+//     return x+y;
+// }
+// console.log("Hi there");
+// setTimeout(()=> console.log("Sorry I'm late"),0);
+// console.log(add(4,5));
